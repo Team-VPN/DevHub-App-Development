4ceb06eb8a3a37bf1fef0b703a56d743e8085a17
Niran N <submachinerox@gmail.com>
2020-07-20 22:37:17 +0530

    fragment/color update
    Issue of icons resolved partially

--- DevHub App/app/src/main/java/com/teamvpn/devhub/MainChat.kt
+++ DevHub App/app/src/main/java/com/teamvpn/devhub/MainChat.kt
@@ -2,10 +2,22 @@ package com.teamvpn.devhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main_chat.*
 
 class MainChat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_chat)
+        setSupportActionBar(toolbar1)
+
+        val toolbar1: Toolbar = findViewById(R.id.toolbar1)
+        setSupportActionBar(toolbar1)
+        supportActionBar!!.title=""
+
+        val tabLayout: TabLayout = findViewById(R.id.tablyt)
+        val viewPager: ViewPager = findViewById(R.id.view_pager1)
     }
 }
