5faf4db881b1ec456cb8fdf1083ae28a1a4b2ca4
Niran N <submachinerox@gmail.com>
2020-07-22 19:48:15 +0530

    Create UserAdapter.kt

---
+++ DevHub App/app/src/main/java/com/teamvpn/devhub/AdapterClasses/UserAdapter.kt
@@ -0,0 +1,64 @@
+package com.teamvpn.devhub.AdapterClasses
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.teamvpn.devhub.ModelClass.Users
+import com.teamvpn.devhub.R
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.activity_main_chat.*
+
+class UserAdapter (
+    mContext: Context,
+    mUsers: List<Users>,
+    isChatCheck: Boolean
+    ) : RecyclerView.Adapter<UserAdapter.ViewHolder?>()
+{
+
+    private val mContext: Context
+    private val mUsers: List<Users>
+    protected var isChatCheck: Boolean
+
+    init {
+        this.mUsers = mUsers
+        this.mContext = mContext
+        this.isChatCheck = isChatCheck
+    }
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view:View = LayoutInflater.from(mContext).inflate(R.layout.user_search_item_layout, viewGroup, false)
+        return UserAdapter.ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return mUsers.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, i: Int) {
+        val user: Users? = mUsers[i]
+        holder.userNameTxt.text = user!!.getUserName()
+        Picasso.get().load(user.getProfile()).placeholder(R.drawable.profile).into(holder.profileImageView)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        var userNameTxt: TextView
+        var profileImageView: CircleImageView
+        var onlineImageView: CircleImageView
+        var offlineImageView: CircleImageView
+        var lastMessageTxt: TextView
+
+        init{
+            userNameTxt = itemView.findViewById(R.id.susername)
+            profileImageView = itemView.findViewById(R.id.sprofile_image)
+            onlineImageView = itemView.findViewById(R.id.image_online)
+            offlineImageView = itemView.findViewById(R.id.image_offline)
+            lastMessageTxt = itemView.findViewById(R.id.message_last)
+        }
+    }
+
+
+}
