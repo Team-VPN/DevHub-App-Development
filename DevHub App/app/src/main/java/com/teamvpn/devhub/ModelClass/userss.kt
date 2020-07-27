5db70dc0319ce51ef3107cfafaf752d3b8df30b9
T_Prathyusha <65026677+T-Prathyusha@users.noreply.github.com>
2020-07-27 21:40:28 +0530

    edit profile

--- DevHub App/app/src/main/java/com/teamvpn/devhub/ModelClass/userss.kt
+++ DevHub App/app/src/main/java/com/teamvpn/devhub/ModelClass/userss.kt
@@ -1,15 +1,31 @@
 package com.teamvpn.devhub.ModelClass
 
-object userss{
-    lateinit var uid:String
-    var username: String = ""
-    var firstname: String = ""
-    var lastname: String = ""
-    var sex:String =""
-     var dob: String=""
-     var phoneNumber: String=""
-     var email: String=""
-    var github : String=""
-     lateinit var skills:MutableList<String>
-     var image_url:String=""
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import de.hdodenhof.circleimageview.CircleImageView
+
+data class userss(
+    var uid: String? = "",
+    var username: String? = "",
+    var fullname: String? = "",
+    var phoneno: String? = "",
+    var email: String? = "",
+    var github: String? = "",
+    var skills: MutableList<String>,
+    var image: String? = ""
+) {
+
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "uid" to uid,
+            "username" to username,
+            "fullname" to fullname,
+            "phoneNumber" to phoneno,
+            "email" to email,
+            "github" to github,
+            "skills" to skills,
+            "imageUrl" to image
+        )
+    }
 }
--- DevHub App/app/src/main/java/com/teamvpn/devhub/RegisterActivity.kt
+++ DevHub App/app/src/main/java/com/teamvpn/devhub/RegisterActivity.kt
@@ -224,6 +224,7 @@ class RegisterActivity : AppCompatActivity() {
                     }
 
                 }
+                //Log.d("Skills check","List"+skillsSelected)
 
                 mskillbuilder.setPositiveButton("add these skills") { _, _ ->
                     Toasty.normal(
