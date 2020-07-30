3342ec6d7156ea791273e285affdcfd9e2906723
Niran N <submachinerox@gmail.com>
2020-07-31 03:48:17 +0530

    chat back button bug fix

--- DevHub App/app/src/main/java/com/teamvpn/devhub/MessageChatActivity.kt
+++ DevHub App/app/src/main/java/com/teamvpn/devhub/MessageChatActivity.kt
@@ -54,9 +54,7 @@ class MessageChatActivity : AppCompatActivity() {
         supportActionBar!!.title = ""
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         toolbar.setNavigationOnClickListener{
-            val intent = Intent(this@MessageChatActivity, MainChat::class.java)//could be unstable
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
+            
             finish()
         }
 
