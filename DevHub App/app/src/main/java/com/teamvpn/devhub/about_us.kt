c0308da89844c4ff65c2e63c13cfa9081578c384
T_Prathyusha <65026677+T-Prathyusha@users.noreply.github.com>
2020-07-31 20:46:08 +0530

    contact us

--- DevHub App/app/src/main/java/com/teamvpn/devhub/about_us.kt
+++ DevHub App/app/src/main/java/com/teamvpn/devhub/about_us.kt
@@ -1,18 +1,57 @@
 package com.teamvpn.devhub
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_about_us.*
+import java.util.jar.Manifest
 
 class about_us : AppCompatActivity() {
 
     val contactNumber = 7795330913
-    val emailAddresses = arrayOf("prathyushathimmapuram@gmail.com")
-    val websiteUrl = "https://www.thinkfinitylabs.com/"
+    val emailAddressespats = arrayOf("prathyushathimmapuram@gmail.com")
+    val emailAddressesniran = arrayOf("hotkid@niran.dev")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
+
+        contact_vallabha.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.setData(Uri.parse("tel:"+contactNumber))
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // so if the permission is not granted this statement is executed based on the condition
+                ActivityCompat.requestPermissions(this@about_us, arrayOf(android.Manifest.permission.CALL_PHONE),1)
+            }else{
+                startActivity(intent)
+            }
+
+        }
+        contact_prathyusha.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.data = Uri.parse("Email")
+            intent.putExtra(Intent.EXTRA_EMAIL,emailAddressespats)
+            intent.putExtra(Intent.EXTRA_SUBJECT,"Support")
+            intent.type = "message/rfc822"
+            val mailIntent = Intent.createChooser(intent,"Launch Email")
+            startActivity(mailIntent)
+        }
+        contact_niran.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.data = Uri.parse("Email")
+            intent.putExtra(Intent.EXTRA_EMAIL,emailAddressesniran)
+            intent.putExtra(Intent.EXTRA_SUBJECT,"Support")
+            intent.type = "message/rfc822"
+            val mailIntent = Intent.createChooser(intent,"Launch Email")
+            startActivity(mailIntent)
+        }
     }
 
     override fun onBackPressed() {
